2e2d575f9a051474995a1787a39f937948a498e0
Murat Sutunc <sutunc@gmail.com>
2015-04-21 14:47:19 -0700

    improvements

--- index.js
+++ index.js
@@ -1,65 +1,21 @@
 var exec = require('child_process').execSync;
-var cordovaProjects = [
-    'cordova-amazon-fireos',
-    'cordova-android',
-    'cordova-app-harness',
-    'cordova-app-hello-world',
-    'cordova-bada',
-    'cordova-blackberry',
-    'cordova-browser',
-    'cordova-cli',
-    'cordova-coho',
-    'cordova-docs',
-    'cordova-firefoxos',
-    'cordova-ios',
-    'cordova-js',
-    'cordova-labs',
-    'cordova-lib',
-    'cordova-medic',
-    'cordova-mobile-spec',
-    'cordova-osx',
-    'cordova-plugin-battery-status',
-    'cordova-plugin-camera',
-    'cordova-plugin-console',
-    'cordova-plugin-contacts',
-    'cordova-plugin-device-motion',
-    'cordova-plugin-device-orientation',
-    'cordova-plugin-device',
-    'cordova-plugin-dialogs',
-    'cordova-plugin-file-transfer',
-    'cordova-plugin-file',
-    'cordova-plugin-geolocation',
-    'cordova-plugin-globalization',
-    'cordova-plugin-inappbrowser',
-    'cordova-plugin-media-capture',
-    'cordova-plugin-media',
-    'cordova-plugin-network-information',
-    'cordova-plugin-splashscreen',
-    'cordova-plugin-statusbar',
-    'cordova-plugin-test-framework',
-    'cordova-plugin-vibration',
-    'cordova-plugins',
-    'cordova-plugman',
-    'cordova-qt',
-    'cordova-registry-web',
-    'cordova-registry',
-    'cordova-tizen',
-    'cordova-ubuntu',
-    'cordova-webos',
-    'cordova-weinre',
-    'cordova-windows',
-    'cordova-wp8'
-]
 
 // Create a user token otherwise you'll hit API rate limit pretty fast
 // Link: https://github.com/settings/applications
-var token = '';
-var tokenStr = token ? ' -H "Authorization: token ' + token + '" ' : ' ';
+var token = process.env.ghkey || '',
+    tokenStr;
+
+if (token) {
+    tokenStr = ' -H "Authorization: token ' + token + '" ';
+} else {
+    console.log('\n\x1B[31mWarning: ghkey not set, you might hit API rate limit.\x1B[39m\n');
+    tokenStr = ' ';
+}
 
 function inactiveBranch(owner, repo, duration) {
     var result = [];
     var branches = exec('curl -s' + tokenStr + '"https://api.github.com/repos/'
-        + owner + '/' + repo + '/branches"', 'utf8');
+        + owner + '/' + repo + '/branches"', {encoding: 'utf8'});
     try {
         branches = JSON.parse(branches);
         if (branches.message) throw Error(branches.message);
@@ -69,7 +25,7 @@ function inactiveBranch(owner, repo, duration) {
 
     branches.forEach(function(branch) {
         try {
-            var commit = exec('curl -s' + tokenStr + branch.commit.url, 'utf8');
+            var commit = exec('curl -s' + tokenStr + branch.commit.url, {encoding: 'utf8'});
             commit = JSON.parse(commit);
             if (commit.message) throw Error(commit.message);
         } catch (e) {
@@ -87,9 +43,39 @@ function inactiveBranch(owner, repo, duration) {
     return result;
 }
 
-cordovaProjects.forEach(function(project) {
-    var oneDay=1000*60*60*24;
-    console.log('Inactive Branches for \x1B[32m' + project + '\x1B[39m');
+
+console.log('Fetching a list of repos..');
+
+// get list of msopentech cordova repos
+var msOTPages = exec('curl -s -I' + tokenStr + 'https://api.github.com/users/msopentech/repos', {encoding: 'utf8'});
+
+msOTPages = msOTPages.split('\n').filter(function(line){
+    return ~line.indexOf('Link: ');
+})[0];
+
+msOTPages = msOTPages.split(',')[1];
+msOTPages = msOTPages.match(/page=(\d+)/i)[1];
+
+var msOTRepos = [];
+
+for (var i = 1; i <= msOTPages; i++) {
+    var repos = exec('curl -s' + tokenStr + '"https://api.github.com/orgs/msopentech/repos?page=' + i);
+    repos = JSON.parse(repos);
+    repos = repos.filter(function(repo) {
+        return !!~repo.name.indexOf('cordova');
+    }).map(function(repo) {
+        return repo.name;
+    });
+    msOTRepos = msOTRepos.concat(repos);
+};
+
+// Sort elements
+msOTRepos = msOTRepos.sort();
+
+var oneDay = 1000*60*60*24;
+
+msOTRepos.forEach(function(project) {
+    console.log('Finding stale branches for \x1B[32m' + project + '\x1B[39m');
     var branches = inactiveBranch('msopentech', project, oneDay);
     console.log(branches.join('\n'));
     console.log('');
